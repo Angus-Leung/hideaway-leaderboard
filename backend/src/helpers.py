702d5c8388b6decec4ec884fb9908633fc5262c1
Donald <77713078+Dsincennes@users.noreply.github.com>
2023-03-25 03:10:03 -0400

    Update helpers.py

--- backend/src/helpers.py
+++ backend/src/helpers.py
@@ -7,15 +7,18 @@ logger = logging.getLogger()
 def formatRawPlayerData(raw_player_data):
     formatted_player_data = []
     for raw_data in raw_player_data:
-        logger.info(raw_data)
-        player_data = {
-            "elo": raw_data["data"]["elo"],
-            "name": raw_data["data"]["name"],
-            "rankUri": raw_data["data"]["currenttierpatched"].replace(" ", "_")
-            + ("_Rank.png"),
-            "tag": raw_data["data"]["tag"],
-        }
-        formatted_player_data.append(player_data)
+        try:
+            logger.info(raw_data)
+            player_data = {
+                "elo": raw_data["data"]["elo"],
+                "name": raw_data["data"]["name"],
+                "rankUri": raw_data["data"]["currenttierpatched"].replace(" ", "_") 
+                + ("_Rank.png"),
+                "tag": raw_data["data"]["tag"],
+            }
+            formatted_player_data.append(player_data)
+        except Exception:
+            pass
 
     formatted_player_data = sorted(
         formatted_player_data, key=itemgetter("elo"), reverse=True
